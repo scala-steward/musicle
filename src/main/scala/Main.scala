d808ad8b4cf7a9c9bf54b7eb93c83a100ef0348d
Kresten Laust <krestenlaust@gmail.com>
2024-11-30 14:23:44 +0100

    Embed audio and button to change source

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,7 @@ import org.scalajs.dom
   // Laminar initialization
   renderOnDomContentLoaded(dom.document.querySelector("#app"), appElement())
 
+val audioSourceVar: Var[String] = Var("")
 val game: Game = Game()
 
 def appElement(): HtmlElement =
@@ -26,11 +27,28 @@ def gameComponent(): HtmlElement =
       li(guessInput()),
       li(guessInput()),
       li(guessInput()),
-    )
+    ),
+    playButton(),
+    songEmbed(audioSourceVar)
   )
 
 def guessInput(): HtmlElement =
   input(cls := "guess-input",
   )
+
+def songEmbed(songSrc: Var[String]): HtmlElement =
+  audioTag(
+    idAttr := "music",
+    sourceTag(
+      src <-- songSrc.signal,
+      `type` := "audio/ogg",
+    )
+  )
+
+def playButton(): HtmlElement =
+  button("Play",
+    onClick --> { _ => audioSourceVar.set(game.songLibrary.songs.head.sourcePath)}
+  )
+
 
 def msg = "I was compiled by Scala 3. :)"
