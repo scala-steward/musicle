dbf66475c0458cb2654fd25b453113460582f9db
Kresten Laust <krestenlaust@gmail.com>
2024-12-02 00:36:21 +0100

    Add some guess logic

--- src/main/scala/Game/Game.scala
+++ src/main/scala/Game/Game.scala
@@ -3,3 +3,13 @@ package Game
 class Game(actualSong: Song):
   private var guesses: List[Song] = List()
 
+  def isGuessed(guessedSong: Song): Boolean =
+    guesses.contains(guessedSong)
+
+  def guessSong(guessedSong: Song): Boolean = {
+    if actualSong == guessedSong then
+      true
+    else
+      guesses = guesses :+ guessedSong // Append guessedSong to guesses immutably
+      false
+  }
